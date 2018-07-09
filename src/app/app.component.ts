f8a358e7cf3abf4836216deacbf159ec8b889f10
Christopher Castaneda <Castanedax26@gmail.com>
2018-07-09 16:04:43 -0500

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = "Christopher";
-  todoInput = '';
+  todoInput: string = "";
   todos = [];
-// User Inputs Name -> YourName's To Do List
 
-//Creates to do list
+
+
 createTodo() {
-  this.todos.push(this.todoInput);
-  this.todoInput="";
+  let trimmedInput = this.todoInput.trim();
+  if (trimmedInput != ""){
+    this.todos.push(this.todoInput);
+  }
+  this.todoInput = "";
 }
-//Edit to do list
+
 editTodo(chore) {
   console.log('Chore that was clicked' + chore);
   let index = this.todos.indexOf(chore);
   console.log('Index of that chore:' + index);
 
-  this.todos[index] = prompt('New To Do:');
+  this.todos[index] = prompt('New To Do:', this.todos[index]);
 }
-//Delete to do
+
 deleteTodo(chore) {
   console.log('Chore that was clicked' + chore);
   let index = this.todos.indexOf(chore);
   this.todos.splice(index, 1);
+  }
+  deleteAllTodos() {
+    this.todos = [];
   }
 }
